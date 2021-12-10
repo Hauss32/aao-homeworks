4335f4c8af702d265e6928f615e5c04717d3de54
Chris Hauss <chris.a.hauss@gmail.com>
2021-12-10 12:14:57 -0800

    Make move tree all possible moves and initialize tree

---
+++ 10Dec21/knights-travails-pt2/knight-path-finder.rb
@@ -0,0 +1,63 @@
+require_relative "poly-tree-node"
+
+class KnightClassFinder
+    def self.valid_moves(pos)
+        board_size = 8
+        vert_moves = [-2,2].product([-1,1])
+        horiz_moves = [-1,1].product([-2,2])
+        all_moves = vert_moves + horiz_moves
+        valid_moves = []
+
+        all_moves.each do |move|
+            new_y = pos[0] + move[0]
+            new_x = pos[1] + move[1]
+            valid_idxs = (0...board_size).to_a
+            valid_move = valid_idxs.include?(new_y) && valid_idxs.include?(new_x)
+            
+            valid_moves << [new_y, new_x] if valid_move
+        end
+
+        valid_moves
+    end
+
+    attr_reader :root_node
+
+    def initialize(pos)
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = []
+
+        self.build_move_tree
+    end
+
+    def new_move_positions(pos)
+        valid_positions = KnightClassFinder.valid_moves(pos)
+        valid_and_new = valid_positions.select { |pos| !@considered_positions.include?(pos) }
+        @considered_positions += valid_and_new
+
+        valid_and_new
+    end
+
+    def build_move_tree
+        queue = [self.root_node]
+
+        until queue.empty?
+            node = queue.shift
+
+            new_moves = self.new_move_positions(node.value)
+
+            child_nodes = new_moves.map do |pos| 
+                child = PolyTreeNode.new(pos)
+                child.parent = node
+            end
+
+            queue += node.children
+        end
+
+        nil
+    end
+
+end
+
+p kcf = KnightClassFinder.new([0,0])
+p kcf.root_node
+
