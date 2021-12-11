5da65727238ff71bafcec871eb6fe412fd44f30b
Chris Hauss <chris.a.hauss@gmail.com>
2021-12-11 12:13:36 -0800

    Add recursice method to find losing node

--- 11Dec21/skeleton/lib/tic-tac-toe-node.rb
+++ 11Dec21/skeleton/lib/tic-tac-toe-node.rb
@@ -1,11 +1,26 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+    attr_reader :board, :next_mover_mark, :prev_move_pos
 
     def initialize(board, next_mark, prev_pos=nil)
         @board = board
         @next_mover_mark = next_mark
-        @prev_pos = prev_pos
+        @prev_move_pos = prev_pos
+    end
+
+    def losing_node?(mark)
+        return true if @board.over? && @board.winner && @board.winner != mark
+
+        if @next_mover_mark != mark
+            lose = self.children.any? { |child| child.losing_node?(mark) }
+            return true if lose
+        else
+            lose = self.children.all? { |child| child.losing_node?(mark) }
+            return true if lose
+        end
+
+        false
     end
 
     def children
@@ -29,9 +44,9 @@ class TicTacToeNode
     end
 end
 
-hp = HumanPlayer.new("Ned")
-cp = ComputerPlayer.new
-ttt = TicTacToe.new(hp, cp)
-n = TicTacToeNode.new(ttt.board, :x)
-p n
-p n.children
+# hp = HumanPlayer.new("Ned")
+# cp = ComputerPlayer.new
+# ttt = TicTacToe.new(hp, cp)
+# n = TicTacToeNode.new(ttt.board, :x)
+# p n
+# p n.children
