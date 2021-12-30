9993055ed07392290dffda65f3a6262548e53faf
Chris Hauss <chris.a.hauss@gmail.com>
2021-12-30 13:53:45 -0800

    Add method to get move from player

--- 29Dec21/lib/humanplayer.rb
+++ 29Dec21/lib/humanplayer.rb
@@ -6,6 +6,34 @@ class HumanPlayer
     end
 
     def make_move
-        
+        cursor = @display.cursor
+        board = @display.board
+
+        selected_pos = nil
+
+        until selected_pos && board[selected_pos].color == @color
+            @display.render
+            puts "\nSelect a #{@color} piece."
+
+            selected_pos = cursor.get_input 
+        end
+
+        move_to = nil
+
+        until move_to && board[move_to].class == NullPiece
+            @display.render
+            puts "\nSelect an empty cell to move to."
+
+            move_to = cursor.get_input 
+        end
+
+        begin
+            board.move_piece(selected_pos, move_to)
+        rescue => exception
+            puts exception
+            return false
+        end
+
+        true
     end
 end
