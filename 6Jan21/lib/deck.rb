4c7f8f5194c922df7298d0150873011e7063cee0
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-06 15:10:03 -0800

    Add init to Deck and add method to take card

---
+++ 6Jan21/lib/deck.rb
@@ -0,0 +1,30 @@
+class Deck
+    def initialize
+        @cards = []
+
+        make_deck
+        shuffle
+    end
+
+    def take
+        raise StandardError.new("No cards left to take!") if @cards.empty?
+
+        @cards.pop
+    end
+
+    private
+    def make_deck
+        suits = ['H', 'S', 'D', 'C']
+        cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+        cards.each do |card_val|
+            suits.each do |suit|
+                @cards << card_val + suit
+            end
+        end
+    end
+
+    def shuffle
+        @cards.shuffle!
+    end
+end
--- 6Jan21/spec/deck_spec.rb
+++ 6Jan21/spec/deck_spec.rb
@@ -6,17 +6,31 @@ describe Deck do
 
     describe '#initialize' do
         it 'starts @cards with an array of 52 cards' do
-            expect(subject.cards.length).to eq(52)
+            subject_cards = subject.instance_variable_get(:@cards)
+            expect(subject_cards.length).to eq(52)
+        end
+
+        it '@cards array has been shuffled into a random order' do
+            original_cards = subject.instance_variable_get(:@cards)
+            sorted = original_cards.sort
+            expect(original_cards).to_not eq(sorted)
         end
     end
 
 
     describe '#take' do
+        let(:card_1) { double("card_1") }
+        let(:card_2) { double("card_2") }
+
         context 'when @cards is not empty' do
-            it 'removes an element from @cards and returns it' do
-                expect(subject.take).to be_a(Card)
+            it 'removes the last element from @cards and returns it' do
+                subject.instance_variable_set(:@cards, [card_1, card_2])
+                expect(subject.take).to eq(card_2)
+                expect(subject.instance_variable_get(:@cards).length).to eq(1)
             end
         end
+
+
 
         context 'when @cards is empty' do
             let(:empty_deck) { Deck.new }
@@ -24,14 +38,6 @@ describe Deck do
                 empty_deck.instance_variable_set(:@cards, [])
                 expect { empty_deck.take }.to raise_error("No cards left to take!")
             end
-        end
-    end
-
-    describe '#shuffle' do
-        it 'randomly reorders the @cards array' do
-            original_cards = subject.cards.dup
-            subject.shuffle
-            expect(subject.cards).to_not eq(original_cards)
         end
     end
 
