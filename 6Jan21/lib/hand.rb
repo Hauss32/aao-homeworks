87990acc2df0405e9e8e14f9e7c3b21594f1c4b1
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-11 18:59:21 -0800

    Add merhod to return numeric hand rank

--- 6Jan21/lib/hand.rb
+++ 6Jan21/lib/hand.rb
@@ -3,6 +3,19 @@ require_relative 'Card'
 class Hand
     include Comparable
 
+    HAND_RANKINGS = {
+        :royal_flush => 10,
+        :straight_flush => 9,
+        :four_of_a_kind => 8,
+        :full_house => 7,
+        :flush => 6,
+        :straight => 5,
+        :three_of_a_kind => 4,
+        :two_pair => 3,
+        :pair => 2,
+        :high_card => 1
+    }
+
     attr_reader :cards
 
     def initialize
@@ -36,11 +49,25 @@ class Hand
     end
 
     # private
+    def hand_rank
+        ranking_methods = Hand::HAND_RANKINGS.keys
+
+        count_card_values
+
+        ranking_methods.each do |method|
+            has_hand_rank = self.send(method)
+
+            if has_hand_rank
+                rank_value = Hand::HAND_RANKINGS[method]
+                return rank_value
+            end
+        end
+    end
 
     def count_card_values
         @count_card_values_hash = Hash.new { |h, k| h[k] = 0 }
 
-        @cards.each { |card| @count_card_values_hash[card.card_value] += 1 }
+        @cards.each { |card| @count_card_values_hash[card.value] += 1 }
 
         true
     end
@@ -65,7 +92,7 @@ class Hand
 
     def flush
         first_card_suit = @cards[0].suit
-        all_same_suit = @cards.all? { |card| card.suit == first_suit }
+        all_same_suit = @cards.all? { |card| card.suit == first_card_suit }
 
         all_same_suit ? true : false
     end
@@ -74,7 +101,8 @@ class Hand
         card_vals = @count_card_values_hash.keys
         return false unless card_vals.length == 5
 
-        card_vals.sort
+        card_vals.sort!
+
         (0...card_vals.length - 1).each do |idx|
             val = card_vals[idx]
             next_val = card_vals[idx + 1]
@@ -97,6 +125,10 @@ class Hand
 
     def pair
         find_pairs.keys.length > 0 ? true : false
+    end
+
+    def high_card
+        true #hand will always have high card
     end
 
     def has_royal_cards
