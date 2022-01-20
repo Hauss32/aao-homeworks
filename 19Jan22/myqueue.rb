205fa9b8bbd87aedf074b09b1110e9536ade5c24
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-19 17:45:50 -0800

    Implement basic MyQueue class

---
+++ 19Jan22/myqueue.rb
@@ -0,0 +1,25 @@
+class MyQueue
+  def initialize
+    @store = []
+  end
+
+  def peek
+      @store[0]
+  end
+
+  def size
+      @store.size
+  end
+
+  def empty?
+      @store.empty
+  end
+
+  def enqueue(num)
+      @store << num
+  end
+
+  def dequeue
+      @store.unshift
+  end
+end
