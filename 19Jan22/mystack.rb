8a0ab68a0472e1770d7ed38dbbbae742e7f12e25
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-19 17:52:10 -0800

    Implement basic MyStack class

---
+++ 19Jan22/mystack.rb
@@ -0,0 +1,25 @@
+class MyStack
+  def initialize
+    @store = []
+  end
+
+  def peek
+      @store[-1]
+  end
+
+  def size
+      @store.size
+  end
+
+  def empty?
+      @store.empty?
+  end
+
+  def push(num)
+      @store << num
+  end
+
+  def pop
+      @store.pop
+  end
+end
