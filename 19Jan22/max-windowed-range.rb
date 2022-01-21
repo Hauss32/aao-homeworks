48ee990fe2917399e897dfd619b9faa251556fb6
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-20 16:18:05 -0800

    Add more performant method for windowed_max_range

--- 19Jan22/max-windowed-range.rb
+++ 19Jan22/max-windowed-range.rb
@@ -1,3 +1,5 @@
+require_relative 'minmaxstackqueue'
+
 def windowed_max_range(array, window_size)
     current_max_range = nil
 
@@ -17,3 +19,31 @@ end
 # => Loop through each window is O(n)...
 # => ...Array#slice/max/min all seem to be O(n) = 3n = O(n) overall...
 # => ...I think this is O(n^2) time complexity
+
+
+# ———————————————————
+# Armed with a working MinMaxStackQueue, this problem should be much easier. 
+# You'll want to follow the same basic approach as above, but use our new data 
+# structure instead of array slices.
+
+def better_windowed_max_range(array, window_size)
+    min_max_queue = MinMaxStackQueue.new
+    current_max_range = nil
+    i = 0
+    until min_max_queue.size == window_size
+        min_max_queue.enqueue(array[i])
+        i += 1
+    end
+
+    window_range = min_max_queue.max - min_max_queue.min
+    current_max_range = window_range if current_max_range.nil? || window_range > current_max_range
+
+    until i == array.length
+        min_max_queue.dequeue
+        min_max_queue.enqueue(array[i])
+        window_range = min_max_queue.max - min_max_queue.min
+        current_max_range = window_range if window_range > current_max_range
+        i += 1
+    end
+    current_max_range
+end
