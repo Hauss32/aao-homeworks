87691e78b7a6b599feca613190a632c643077ac1
Chris Hauss <chris.a.hauss@gmail.com>
2022-01-26 16:55:19 -0800

    Implement query for nobel_bonus

--- 26Jan22/lib/03a_nobel_bonus.rb
+++ 26Jan22/lib/03a_nobel_bonus.rb
@@ -11,5 +11,17 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT yr
+    FROM nobels
+    WHERE 
+      subject = 'Physics' AND
+      yr IN (
+        SELECT yr 
+        FROM nobels 
+        WHERE subject IN ('Physics', 'Chemistry')
+        GROUP BY 1
+        HAVING COUNT(DISTINCT subject) = 1
+      )
+    GROUP BY 1
   SQL
 end
