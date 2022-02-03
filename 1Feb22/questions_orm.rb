d6c360ed874367a49d3a066ffcdd0509d7b9702b
Chris Hauss <chris.a.hauss@gmail.com>
2022-02-02 17:38:35 -0800

    Implement various query methods on each model class

--- 1Feb22/questions_orm.rb
+++ 1Feb22/questions_orm.rb
@@ -29,7 +29,7 @@ class User
             WHERE fname = ? AND lname = ?
         SQL
 
-        User.new(user)
+        User.new(user.first)
     end
 
     attr_accessor :id, :fname, :lname
@@ -38,6 +38,38 @@ class User
         @id = options['id']
         @fname = options['fname']
         @lname = options['lname']
+    end
+
+    def authored_questions
+        raise "User does not exist." unless @id
+        Question.find_by_author_id(@id)
+    end
+
+    def authored_replies
+        raise "User does not exist." unless @id
+        Reply.find_by_user_id(@id)
+    end
+
+    def followed_questions
+        raise "User does not exist." unless @id
+        QuestionFollow.followed_questions_for_user_id(@id)
+    end
+
+    def count_authored_questions
+        raise "User does not exist." unless @id
+        Question.count_questions_by_author_id(@id)
+    end
+
+    def count_likes_from_authored_questions
+        raise "User does not exist." unless @id
+        QuestionLike.count_likes_for_author_id(@id)
+    end
+
+    def average_karma
+        count_questions = count_authored_questions
+        return 0 unless count_questions > 0
+
+        count_likes_from_authored_questions / count_questions
     end
 end
 
@@ -52,14 +84,67 @@ class Question
         Question.new(question.first)
     end
 
+    def self.find_by_author_id(author_id)
+        questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+            SELECT *
+            FROM questions
+            WHERE author_id = ?
+        SQL
+
+        questions.map { |question| Question.new(question) }
+    end
+
+    def self.count_questions_by_author_id(author_id)
+        count = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+            SELECT COUNT(*)
+            FROM questions
+            WHERE author_id = ?
+        SQL
+
+        count.first.values.first
+    end
+
+    def self.most_followed(n)
+        QuestionFollow.most_followed_questions(n)
+    end
+
+    def self.most_liked(n)
+        QuestionLike.most_liked_questions(n)
+    end
+
     attr_accessor :id, :title, :body, :author_id
 
     def initialize(options)
         @id = options['id']
         @title = options['title']
         @body = options['body']
-        @author_id = ['author_id']
+        @author_id = options['author_id']
     end
+
+    def author
+        User.find_by_id(@author_id)
+    end
+
+    def replies
+        raise "Question does not exist." unless @id
+        Reply.find_by_question_id(@id)
+    end
+
+    def followers
+        raise "Question does not exist." unless @id
+        QuestionFollow.followers_for_question_id(@id)
+    end
+
+    def likers
+        raise "Question does not exist." unless @id
+        QuestionLike.likers_for_question_id(@id)
+    end
+
+    def num_likes
+        raise "Question does not exist." unless @id
+        QuestionLike.num_likes_for_question_id(@id)
+    end
+
 end
 
 class QuestionFollow
@@ -71,6 +156,40 @@ class QuestionFollow
         SQL
 
         QuestionFollow.new(question_follow.first)
+    end
+
+    def self.followers_for_question_id(question_id)
+        question_follows = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM question_follows
+            WHERE question_id = ?
+        SQL
+
+        question_follows.map { |follow| User.find_by_id(follow['user_id']) }
+    end
+
+    def self.most_followed_questions(n)
+        questions = QuestionsDatabase.instance.execute(<<-SQL, n)
+            SELECT
+                question_id,
+                COUNT(*) as followers
+            FROM question_follows
+            GROUP BY 1
+            ORDER BY followers DESC
+            LIMIT ?
+        SQL
+
+        questions.map { |question| Question.find_by_id(question['question_id']) }  
+    end
+
+    def self.followed_questions_for_user_id(user_id)
+        followed_questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT *
+            FROM question_follows
+            WHERE user_id = ?
+        SQL
+
+        followed_questions.map { |question| Question.find_by_id(question['question_id']) }
     end
 
     attr_accessor :id, :question_id, :user_id
@@ -93,6 +212,61 @@ class QuestionLike
         QuestionLike.new(question_like.first)
     end
 
+    def self.likers_for_question_id(question_id)
+        question_likers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM question_likes
+            WHERE question_id = ?
+        SQL
+
+        question_likers.map { |like| User.find_by_id(like['user_id']) }
+    end
+
+    def self.count_likes_for_author_id(author_id)
+        count = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+            SELECT COUNT(*)
+            FROM questions AS q
+            JOIN question_likes AS ql ON q.id = ql.question_id
+            WHERE q.author_id = ?
+        SQL
+
+        count.first.values.first
+    end
+
+    def self.most_liked_questions(n)
+        questions = QuestionsDatabase.instance.execute(<<-SQL, n)
+            SELECT
+                question_id,
+                COUNT(*) as likes
+            FROM question_likes
+            GROUP BY 1
+            ORDER BY likes DESC
+            LIMIT ?
+        SQL
+
+        questions.map { |question| Question.find_by_id(question['question_id']) }  
+    end
+
+    def self.liked_questions_for_user_id(user_id)
+        liked_questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT *
+            FROM question_likes
+            WHERE user_id = ?
+        SQL
+
+        liked_questions.map { |question| Question.find_by_id(question['question_id']) }
+    end
+
+    def self.num_likes_for_question_id(question_id)
+        num_likes = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT COUNT(*)
+            FROM question_likes
+            WHERE question_id = ?
+        SQL
+
+        num_likes.first.values.first
+    end
+
     attr_accessor :id, :question_id, :user_id
 
     def initialize(options)
@@ -113,6 +287,26 @@ class Reply
         Reply.new(reply.first)
     end
 
+    def self.find_by_user_id(user_id)
+        replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT *
+            FROM replies
+            WHERE user_id = ?
+        SQL
+
+        replies.map { |reply| Reply.new(reply) }
+    end
+
+    def self.find_by_question_id(question_id)
+        replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM replies
+            WHERE question_id = ?
+        SQL
+
+        replies.map { |reply| Reply.new(reply) }
+    end
+
     attr_accessor :id, :question_id, :parent_reply_id, :body, :user_id
 
     def initialize(options)
@@ -120,6 +314,30 @@ class Reply
         @question_id = options['question_id']
         @parent_reply_id = options['parent_reply_id']
         @body = options['body']
-        @user_id = ['user_id']
+        @user_id = options['user_id']
+    end
+
+    def author
+        User.find_by_id(@user_id)
+    end
+
+    def question
+        Question.find_by_id(@question_id)
+    end
+
+    def parent_reply
+        return nil unless @parent_reply_id
+        Reply.find_by_id(@parent_reply_id)
+    end
+
+    def child_replies
+        raise "Reply does not exist." unless @id
+        replies = QuestionsDatabase.instance.execute(<<-SQL, @id)
+            SELECT *
+            FROM replies
+            WHERE parent_reply_id = ?
+        SQL
+
+        replies.map { |reply| Reply.new(reply) }
     end
 end
