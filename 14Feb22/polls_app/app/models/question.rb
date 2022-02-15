1d023ff420fbedc465f86890e1b9c1fa8fdaa133
Chris Hauss <chris.a.hauss@gmail.com>
2022-02-15 15:16:36 -0800

    Add Response#sibling_responses

--- 14Feb22/polls_app/app/models/question.rb
+++ 14Feb22/polls_app/app/models/question.rb
@@ -28,4 +28,8 @@ class Question < ApplicationRecord
         class_name: :AnswerChoice,
         primary_key: :id, 
         foreign_key: :question_id
+
+    has_many :responses,
+        through: :answer_choices,
+        source: :responses
 end
