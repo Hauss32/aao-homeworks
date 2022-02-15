3577cfa3c9134d22bfcdaa936241bae64fb6ffe5
Chris Hauss <chris.a.hauss@gmail.com>
2022-02-14 17:57:50 -0800

    Add small seed file

--- 14Feb22/polls_app/db/seeds.rb
+++ 14Feb22/polls_app/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Response.destroy_all
+AnswerChoice.destroy_all
+Question.destroy_all
+Poll.destroy_all
+User.destroy_all
+
+
+User.create!(username: 'NormalChris')
+User.create!(username: 'EvilChris')
+
+Poll.create!(title: 'Best Users', user_id: User.first.id)
+
+Question.create!(question_text: 'Which User do you like better?', poll_id: Poll.first.id)
+
+AnswerChoice.create!(choice_text: 'NormalChris', question_id: Question.first.id)
+AnswerChoice.create!(choice_text: 'EvilChris', question_id: Question.first.id)
+
+Response.create!(answer_choice_id: AnswerChoice.last.id, user_id: User.last.id)
+
+
+
