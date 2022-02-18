e6d03bf76a25aa0027f32db47a75e2ab1eea951e
Chris Hauss <chris.a.hauss@gmail.com>
2022-02-18 12:03:20 -0800

    Make Searchable module with where mothod to exten SQLObject

--- 17Feb22/lib/02_searchable.rb
+++ 17Feb22/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_cols = params.keys.map { |col| col.to_s + ' = ?' } 
+    where_str = where_cols.join(' AND ')
+    values = params.values
+
+    records = DBConnection.execute(<<-SQL, *values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{where_str}
+    SQL
+
+    records.map { |record| self.new(record) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
