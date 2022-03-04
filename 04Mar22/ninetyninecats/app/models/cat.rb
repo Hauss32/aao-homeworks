b954c2858d2a02fea3ed1a83549178f13df8c799
Chris Hauss <chris.a.hauss@gmail.com>
2022-03-04 11:47:50 -0800

    Add cat data to seed

--- 04Mar22/ninetyninecats/app/models/cat.rb
+++ 04Mar22/ninetyninecats/app/models/cat.rb
@@ -5,7 +5,7 @@ class Cat < ApplicationRecord
 
     COLORS = ['White', 'Orange', 'Brown', 'Black', 'Mix']
 
-    validates :birth_date, :color, :name, :sex, :description, presense: true
+    validates :birth_date, :color, :name, :sex, :description, presence: true
     validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color" }
     validates :sex, inclusion: { in: ['M', 'F'], message: "Sex must be 'M' or 'F'" }
 
