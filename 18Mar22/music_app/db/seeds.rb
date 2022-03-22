e662d240473f5263bd3adcce793be1f61a73dc5b
Chris Hauss <chris.a.hauss@gmail.com>
2022-03-21 18:00:58 -0700

    Add seed file with User, Band, Album, and Tracks

--- 18Mar22/music_app/db/seeds.rb
+++ 18Mar22/music_app/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: 'someuser@musicapp.test', password: 'password')
+
+band = Band.create(name: 'The Rubber Band')
+
+album = Album.create(
+    title: 'Rubber Band Man', 
+    year: 1969, 
+    is_live_album: true,
+    band_id: band.id)
+
+tracks = [
+    'Captain Elastic', 
+    'Slingshot Hotshot', 
+    'Stretch for the Moon',
+    'Snappy',
+    'Stretch it!']
+
+tracks.each_with_index do |track, idx|
+    Track.create(
+        title: track,
+        ord: idx+1,
+        lyrics: '',
+        album_id: album.id)
+end
+
+Track.last.update(is_bonus_track: true)
