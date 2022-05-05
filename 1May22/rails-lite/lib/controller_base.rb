76065d7a23d3acee632bd67db1a44a89a0d2a0ed
Chris Hauss <chris.a.hauss@gmail.com>
2022-05-04 18:37:46 -0700

    Implement Flash and Flash.now

--- 1May22/rails-lite/lib/controller_base.rb
+++ 1May22/rails-lite/lib/controller_base.rb
@@ -3,14 +3,16 @@ require 'active_support/core_ext'
 require 'active_support/inflector'
 require 'erb'
 require_relative './session'
+require_relative './flash'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params={})
     @req = req
     @res = res
+    @params = route_params.merge!(req.params)
   end
 
   # Helper method to alias @already_built_response
@@ -23,7 +25,8 @@ class ControllerBase
     raise 'Content already rendered.' if already_built_response?
     @res.redirect(url, 302)
     @already_built_response = true
-    @session.store_session(@res)
+    session.store_session(@res)
+    flash.store_flash(@res)
   end
 
   # Populate the response with content.
@@ -31,15 +34,18 @@ class ControllerBase
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise 'Content already rendered.' if already_built_response?
+    flash.store_flash(@res)
     @res['Content-Type'] = content_type
     @res.write(content)
     @already_built_response = true
-    @session.store_session(@res)
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    puts flash[:errors]
+    p flash
     class_name = self.class.name.underscore
     file_dir = File.dirname(__FILE__)
     path = File.join(file_dir.to_s, 'views', class_name, template_name.to_s+'.html.erb')
@@ -52,8 +58,14 @@ class ControllerBase
     @session ||= Session.new(@req)
   end
 
+  def flash
+    @flash ||= Flash.new(@req)
+  end
+
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
 
--- 1May22/rails-lite/lib/flash.rb
+++ 1May22/rails-lite/lib/flash.rb
@@ -1,4 +1,29 @@
 require 'json'
 
 class Flash
+    attr_reader :now
+
+    def initialize(req)
+        @flash = {}
+        @now = get_or_create_flash(req)
+    end
+
+    def [](key)
+        @now[key.to_s] || @flash[key.to_s]
+    end
+
+    def []=(key, value)
+        @flash[key] = value
+    end
+
+    def store_flash(res)
+        flash = @flash.to_json
+        res.set_cookie('_rails_lite_app_flash', { path: '/', value: flash})
+    end
+
+    private
+    def get_or_create_flash(req)
+        flash = req.cookies['_rails_lite_app_flash']
+        flash ? JSON.parse(flash) : {}
+    end
 end
