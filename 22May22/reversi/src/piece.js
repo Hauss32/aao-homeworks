78bc65a5701b783c09582808d5b8a5068654a94d
Chris Hauss <chris.a.hauss@gmail.com>
2022-05-22 11:15:35 -0700

    Implement Piece class

--- 22May22/reversi/src/piece.js
+++ 22May22/reversi/src/piece.js
@@ -2,18 +2,27 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    const COLORS = ['black', 'white']
+
+    if(COLORS.includes(color)){
+        this.color = color;
+    } else{
+        throw `${color} is not a valid piece color (black, white)`;
+    }
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    return this.color === 'white' ? 'black' : 'white';
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color = this.oppColor();
 };
 
 /**
@@ -21,6 +30,7 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    return this.color === 'white' ? 'W' : 'B';
 };
 
 // DON'T TOUCH THIS CODE
