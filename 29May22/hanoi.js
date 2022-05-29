fa3452484996350cebca297031d8bc6790e290b0
Chris Hauss <chris.a.hauss@gmail.com>
2022-05-29 10:34:49 -0700

    Initial commit for Towers of Hanoi

---
+++ 29May22/hanoi.js
@@ -0,0 +1,14 @@
+class Hanoi {
+    constructor() {
+
+    }
+
+    run() {
+        //until pieces are sequenced correctly on last tower
+        //print board
+        //ask user for move
+        //move piece
+
+        //congratulate winner when game over
+    }
+}
