d95edb020491c0d08f3effc21686fc03a759febc
Chris Hauss <chris.a.hauss@gmail.com>
2022-06-08 16:51:33 -0700

    Implement draw and move methods for MovingObject class

---
+++ 8Jun22/src/index.js
@@ -0,0 +1,12 @@
+const MovingObject = require('./moving_object');
+
+window.MovingObject = MovingObject;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const canv = document.getElementById('game-canvas');
+    const ctx = canv.getContext("2d");
+    window.clearCanv = () => {
+        ctx.clearRect(0, 0, canv.width, canv.height);
+    };
+    window.ctx = ctx;
+});
---
+++ 8Jun22/src/moving_object.js
@@ -0,0 +1,28 @@
+function MovingObject(options) {
+    this.pos = options.pos;
+    this.vel = options.vel;
+    this.radius = options.radius;
+    this.color = options.color;
+    this.ctx = options.ctx;
+}
+
+MovingObject.prototype.draw = function() {
+    this.ctx.beginPath();
+    this.ctx.arc(
+        this.pos[0],
+        this.pos[1],
+        this.radius,
+        0,
+        2 * Math.PI
+    );
+
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+}
+
+MovingObject.prototype.move = function() {
+    this.pos[0] += this.vel[0];
+    this.pos[1] += this.vel[1];
+}
+
+module.exports = MovingObject;
