3f7bba5743e62a28a0b8fe9e6e25cfdf69eea92a
Chris Hauss <chris.a.hauss@gmail.com>
2022-07-15 10:10:40 -0600

    Implement DomNodeCollection append method

--- 15Jul22/src/dom_node_collection.js
+++ 15Jul22/src/dom_node_collection.js
@@ -18,6 +18,26 @@ class DomNodeCollection {
             elem.innerHTML = '';
         })
     }
+
+    append(data) {
+        if (data instanceof DomNodeCollection) {
+            data.collection.forEach( elem => {
+                const dataToAppend = elem.outerHTML;
+
+                this.collection.forEach( domElem => {
+                    domElem.innerHTML += dataToAppend;
+                })
+            })
+        } else if (data instanceof HTMLElement) {
+            this.collection.forEach( domElem => {
+                domElem.innerHTML += data.outerHTML;
+            } )    
+        } else {
+            this.collection.forEach(domElem => {
+                domElem.innerHTML += data;
+            })   
+        }
+    }
 }
 
 module.exports = DomNodeCollection;
