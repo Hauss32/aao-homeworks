fe8cc95e4cb8dd6a1e54a28e0bd6d0f0dad21d3b
Chris Hauss <chris.a.hauss@gmail.com>
2022-07-15 09:31:41 -0600

    Add DomNodeCollection class and have $l function accept html elem

---
+++ 15Jul22/src/dom_node_collection.js
@@ -0,0 +1,7 @@
+class DomNodeCollection {
+    constructor(collection) {
+        this.collection = collection;
+    }
+}
+
+module.exports = DomNodeCollection;
--- 15Jul22/src/index.js
+++ 15Jul22/src/index.js
@@ -1,3 +1,14 @@
+const DomNodeCollection = require('./dom_node_collection'); 
+
 window.$l = function(selector) {
-    return Array.from( document.querySelectorAll(selector) );
+    if ( selector instanceof HTMLElement) {
+        return new DomNodeCollection( [selector] );
+    } else {
+        const htmlArr = Array.from( document.querySelectorAll(selector) );
+        return new DomNodeCollection( htmlArr );
+    }
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    // DOM ready code
+});
