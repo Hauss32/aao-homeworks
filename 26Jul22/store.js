e740bf72587e1031df149468789d6378bc46e4b7
Chris Hauss <chris.a.hauss@gmail.com>
2022-07-26 15:01:45 -0600

    Update combineReducers function to close over mapping of reducers

--- 26Jul22/store.js
+++ 26Jul22/store.js
@@ -7,18 +7,63 @@ class Store {
     getState() {
         return structuredClone(this.state);
     }
+
+    dispatch(actions) {
+
+    }
 }
 
 function combineReducers(reducerMapping) {
     return (prevState, action) => {
         const newState = {};
 
-        Object.keys(prevState).forEach(key => {
+        Object.keys(reducerMapping).forEach( key => {
             const prevVal = prevState[key];
             const reducer = reducerMapping[key];
             const result = reducer(prevVal, action);
             newState[key] = result;
-        })
+        } )
         return newState;
     }
 }
+
+const myNoiseReducer = (prevState = "peace and quiet", action) => {
+    switch (action.type) {
+        case "noisy action":
+            return action.noise;
+        default:
+            return prevState;
+    }
+};
+
+const myNoisyAction = {
+    type: "noisy action",
+    noise: "Car alarm"
+};
+
+const myInconsequentialAction = {
+    type: "a type no one cares about",
+    data: {
+        thisThing: "will not get used anyway"
+    }
+};
+
+const myInitialState = {
+    noise: "peace and quiet"
+};
+
+const myRootReducer = combineReducers({
+    noise: myNoiseReducer,
+});
+
+let newState = myRootReducer(myInitialState, myInconsequentialAction);
+// => { noise: "peace and quiet" }
+console.log(newState);
+
+newState = myRootReducer(newState, myNoisyAction)
+// => { noise: "Car alarm" }
+console.log(newState);
+
+myRootReducer(newState, myInconsequentialAction)
+// => { noise: "Car alarm" }
+console.log(newState);
