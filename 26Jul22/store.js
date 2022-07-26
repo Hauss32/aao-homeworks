e13eaf636d5a842857880392815304d99c0cb6f2
Chris Hauss <chris.a.hauss@gmail.com>
2022-07-26 14:26:28 -0600

    Implement combineReducers method

--- 26Jul22/store.js
+++ 26Jul22/store.js
@@ -7,4 +7,18 @@ class Store {
     getState() {
         return structuredClone(this.state);
     }
+
+    combineReducers( reducerMapping ) {
+        const newState = {};
+
+        return ( prevState, action ) => {
+            Object.keys( prevState ).forEach( key => {
+                const prevVal = prevState[key];
+                const reducer = reducerMapping[key];
+                const result = reducer( prevVal, action );
+                newState[key] = result;
+            })
+            return newState;
+        }
+    }
 }
